c48b22c91f91f06a297feca67915dd6105453f37
zhalok <zhalokrahman007@gmail.com>
2025-02-14 22:12:06 +0600

    changed orders output structure

--- src/order/dto/get-orders.output.ts
+++ src/order/dto/get-orders.output.ts
@@ -1,11 +1,23 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, Float, ObjectType } from '@nestjs/graphql';
 import { Pagination } from 'src/common/dtos/pagination.dto';
 import { Order } from '../entities/order.entity';
+
+@ObjectType()
+class Orderdata {
+  @Field(() => String)
+  id: string;
+
+  @Field(() => Float)
+  totalAmount: number;
+
+  @Field(() => String)
+  status: string;
+}
 
 @ObjectType()
 export class GetOrdersOutput {
   @Field(() => Pagination)
   pagination: Pagination;
-  @Field(() => [Order])
-  data: Order[];
+  @Field(() => [Orderdata])
+  data: Orderdata[];
 }
